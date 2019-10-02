22505cec54662fba8f2fa51c4f9112a38e3ca84c
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-10-02 04:02:31 -0500

    tried basically everything to line everything to the center. copied examples from w3 and stack overflow with no change. probably a typo on my end somewhere

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,78 +8,84 @@ var diamondCardValue = 0;
 var clubCardValue = 0;
 var jokerCardValue = 0;
 
-
-
 function setObjectivePointsContainer() {
-    $("#objective-points-container").text(objectivePoints);
+  $("#objective-points-container").text(objectivePoints);
 }
 
 function setRecordsContainer() {
-    $("#records-container").text("Wins: " + wins + " Losses: " + losses);
+  $("#records-container").text("Wins: " + wins + " Losses: " + losses);
 }
 
 function setPointsContainer() {
-    $("#points-container").text("Points " + playerPoints);
+  $("#points-container").text("Points " + playerPoints);
 }
 
 function selectNewObjectivePoints() {
-    var randomNumber = Math.floor(Math.random()*101)+19;
-    objectivePoints = randomNumber;
-    setObjectivePointsContainer();
+  var randomNumber = Math.floor(Math.random() * 101) + 19;
+  objectivePoints = randomNumber;
+  setObjectivePointsContainer();
 }
 
 function selectNewCardValues() {
-    heartCardValue = Math.floor(Math.random()*12)+1;
-    spadeCardValue = Math.floor(Math.random()*12)+1;
-    diamondCardValue = Math.floor(Math.random()*12)+1;
-    clubCardValue = Math.floor(Math.random()*12)+1;
-    jokerCardValue = Math.floor(Math.random()*12)+1;
-    console.log(heartCardValue + " " + spadeCardValue + " " + diamondCardValue + " " + clubCardValue + " " + jokerCardValue );
+  heartCardValue = Math.floor(Math.random() * 12) + 1;
+  spadeCardValue = Math.floor(Math.random() * 12) + 1;
+  diamondCardValue = Math.floor(Math.random() * 12) + 1;
+  clubCardValue = Math.floor(Math.random() * 12) + 1;
+  jokerCardValue = Math.floor(Math.random() * 12) + 1;
+  console.log(
+    heartCardValue +
+      " " +
+      spadeCardValue +
+      " " +
+      diamondCardValue +
+      " " +
+      clubCardValue +
+      " " +
+      jokerCardValue
+  );
 }
 
 function addPoints(n) {
-    playerPoints += n;
-    setPointsContainer();
+  playerPoints += n;
+  setPointsContainer();
 
-    if(playerPoints == objectivePoints) {
-        wins++;
-        setRecordsContainer();
-        playerPoints = 0;
-        selectNewObjectivePoints();
+  if (playerPoints == objectivePoints) {
+    wins++;
+    setRecordsContainer();
+    playerPoints = 0;
+    selectNewObjectivePoints();
     selectNewCardValues();
-    }
+  }
 
-    if(playerPoints > objectivePoints) {
-        losses++;
-        setRecordsContainer();
-        playerPoints = 0;
-        setPointsContainer();
-        selectNewObjectivePoints();
-        selectNewCardValues();
-    }
+  if (playerPoints > objectivePoints) {
+    losses++;
+    setRecordsContainer();
+    playerPoints = 0;
+    setPointsContainer();
+    selectNewObjectivePoints();
+    selectNewCardValues();
+  }
 }
 
-$("#heart-card").click(function(){
-    addPoints(heartCardValue);
+$("#heart-card").click(function() {
+  addPoints(heartCardValue);
 });
 
-$("#spade-card").click(function(){
-    addPoints(spadeCardValue);
+$("#spade-card").click(function() {
+  addPoints(spadeCardValue);
 });
 
-$("#diamond-card").click(function(){
-    addPoints(diamondCardValue);
+$("#diamond-card").click(function() {
+  addPoints(diamondCardValue);
 });
 
-$("#club-card").click(function(){
-    addPoints(clubCardValue);
+$("#club-card").click(function() {
+  addPoints(clubCardValue);
 });
 
-$("#joker-card").click(function(){
-    addPoints(jokerCardValue);
+$("#joker-card").click(function() {
+  addPoints(jokerCardValue);
 });
-
-
 
 selectNewObjectivePoints();
 selectNewCardValues();
